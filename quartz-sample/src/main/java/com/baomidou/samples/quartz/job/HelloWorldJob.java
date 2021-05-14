828312da17be5fb93b9e7429047ca559938657e8
tujiuquan <m18646088512@163.com>
2021-05-14 13:59:05 +0800

    去除异常

--- quartz-sample/src/main/java/com/baomidou/samples/quartz/job/HelloWorldJob.java
+++ quartz-sample/src/main/java/com/baomidou/samples/quartz/job/HelloWorldJob.java
@@ -2,7 +2,6 @@ package com.baomidou.samples.quartz.job;
 
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 @Slf4j
@@ -11,7 +10,8 @@ public class HelloWorldJob extends QuartzJobBean {
     private static int time = 0;
 
     @Override
-    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+    protected void executeInternal(JobExecutionContext jobExecutionContext) {
         log.info("Hello world!:" + jobExecutionContext.getJobDetail().getKey() + "-" + (time++));
     }
+
 }
